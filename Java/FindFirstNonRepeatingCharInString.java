0a1ad9622730fa8325c1c6d2566919d1a887b065
Rajesh <k.raj20@gmail.com>
2017-01-14 20:58:13 -0800

    First Repeating charachter and other solutions

---
+++ Java/FindFirstNonRepeatingCharInString.java
@@ -0,0 +1,58 @@
+package Algo.Test;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class FindFirstNonRepeatingCharInString {
+
+	class Tuple {
+		int indexVal;
+		int countVal;
+	}
+	
+	public static void main(String[] args) {
+		
+		String input = "aacbaadce";
+		System.out.println("First unique charachter in string '" + input + " : " + FirstUniqueChar(input));
+
+	}
+	
+	public static char FirstUniqueChar(String str){
+		
+		HashMap<Character, Tuple> map = new HashMap<Character, Tuple>();
+		char myChar = ' ';
+		int lowestIndex = -1;
+		
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            
+            if(map.containsKey(c)){           	
+            	
+            	if(map.get(c).countVal < 2)            		
+            		map.get(c).countVal = 2;
+            }
+            else {
+            	Tuple tup = new FindFirstNonRepeatingCharInString().new Tuple();
+            	tup.indexVal = i;
+            	tup.countVal = 1;
+            	map.put(c, tup);
+            }			
+		}
+        
+        Iterator<Entry<Character, Tuple>> it = map.entrySet().iterator();
+        
+        while (it.hasNext()) {
+        	Map.Entry<Character, Tuple> pair = it.next();
+        	int temp = pair.getValue().countVal;
+        	if(temp == 1 && (lowestIndex > pair.getValue().indexVal || lowestIndex == -1)){
+        		lowestIndex = pair.getValue().indexVal;
+        		myChar = pair.getKey();
+        	}
+        }
+        
+		return myChar;
+	}
+}
--- Java/FindPairInArrayAddUpToNumber.java
+++ Java/FindPairInArrayAddUpToNumber.java
@@ -12,12 +12,9 @@ public class FindPairInArrayAddUpToNumber {
 
 		int[] number = new int[] {-1,-2,-3,-4,-5};
 		int givenNum = -8;
-		int[] out = indexOfPairAddUpToNumber(number,givenNum);
-		System.out.print("Index Of Pair Add Up To Number " + String.valueOf(givenNum) + " : [" + String.valueOf(out[0]) + "," + String.valueOf(out[1]) + "]");
-		
         StringBuilder sb = new StringBuilder();
         givenNum = 12;
-        sb.append("\nFind all the pair of 2 numbers in array, which add up to the number : " + String.valueOf(givenNum));
+        sb.append("Find all the pair of 2 numbers in array, which add up to the number : " + String.valueOf(givenNum));
         sb.append("\nGiven array (value|index) is : { 1|0, 2|1, 6|2, 6|3, 3|4, 4|5, 8|6, 6|7, 4|8, 8|9, 9|10 } is and pairs are\n");
         sb.append("pairAddUpToNumber : ");
         int[] arrayForPairs = new int[] { 1, 2, 6, 6, 3, 4, 8, 6, 4, 8, 9 };
@@ -53,70 +50,6 @@ public class FindPairInArrayAddUpToNumber {
         	
         	System.out.print("],");
         }
-	}
-
-	//Index Of 2 Pair in an array, which add up to the input number
-	//Time complexity depends on the put and get operations of HashMap 
-	//which is normally O(1).
-	//Test Case: {-1,-2,-3,-4,-5} and -8. Output [2,4]
-	//Input: numbers={2, 7, 11, 15}, target=9 
-	//Output: index1=0, index2=1. Time complexity of this solution is O(n)
-	public static int[] indexOfPairAddUpToNumber(int[] nums, int target){
-		
-		if(nums == null || nums.length < 2)
-			return new int[]{-1,-1};;
-		
-		HashMap<Integer, String> map = new HashMap<Integer, String>();
-				
-		for(int i = 0; i < nums.length; i++){
-			
-			if(map.containsKey(nums[i]))
-				map.put(nums[i], map.get(nums[i]) + "," + String.valueOf(i)) ;
-			else
-				map.put(nums[i], String.valueOf(i));
-		}
-		
-		for(int i = 0; i < nums.length; i++){
-			
-			int val = target - nums[i];
-			
-			//This is for duplicate numbers
-			if(val == nums[i] && map.containsKey(val))
-			{
-				String[] iStr = map.get(nums[i]).split(",");
-				
-				if(iStr.length > 1){				
-					for(int j = 0; j < iStr.length; j++){
-						for(int k=(j+1); k < iStr.length; k++){
-							if ((nums[Integer.parseInt(iStr[j])] + nums[Integer.parseInt(iStr[k])]) == target){
-								return new int[]{Integer.parseInt(iStr[j]),Integer.parseInt(iStr[k])};
-							}
-						}
-					}
-				}
-
-				map.remove(val);
-				
-			}
-			else if( val != nums[i] && map.containsKey(val))
-			{
-				String[] iStr = map.get(nums[i]).split(",");
-				String[] iStrVal = map.get(val).split(",");
-				
-			
-				for(int j = 0; j < iStr.length; j++){
-					for(int k = 0; k < iStrVal.length; k++){
-						if ((nums[Integer.parseInt(iStr[j])] + nums[Integer.parseInt(iStrVal[k])]) == target){
-							return new int[]{Integer.parseInt(iStr[j]),Integer.parseInt(iStrVal[k])};
-						}
-					}
-				}
-				
-				map.remove(val);
-			}
-		}
-		
-		return new int[]{-1,-1};
 	}
 	
     // Find all the pair of 2 numbers in array, which add up to the number
