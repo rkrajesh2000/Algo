e276951f7bcc91690b19766578fef22f5ece2966
Rajesh <k.raj20@gmail.com>
2017-01-21 14:31:14 -0800

    Maze Exit Path

--- Java/MazeExitPath.java
+++ Java/MazeExitPath.java
@@ -6,20 +6,28 @@ public class MazeExitPath {
 		// TODO Auto-generated method stub
 		
 		MazeExitPath rat = new MazeExitPath();
-//        int maze[][] = {
-//        					{1, 0, 1, 1, 1},
-//        					{1, 0, 1, 0, 1},
-//        					{1, 1, 1, 0, 1},
-//        					{1, 1, 0, 0, 1}
-//                		};
-
         int maze[][] = {
+        					{1, 0, 1, 1, 1},
+        					{1, 0, 1, 0, 1},
+        					{1, 1, 1, 0, 1},
+        					{1, 1, 0, 0, 1}
+                		};
+
+        int maze2[][] = {
 				{1, 0, 0, 0, 0},
 				{1, 0, 0, 0, 1},
 				{1, 1, 1, 1, 1},
 				{1, 1, 0, 0, 1}
     		};
         if(rat.findExitPath(maze,4, 5)){
+        	System.out.println("Exit path found in maze");
+        }
+        else
+        	System.out.println("Exit path not found in maze");
+        
+        
+        
+        if(rat.findExitPathOnlyRightAndDownMove(maze2,4, 5)){
         	System.out.println("Exit path found in maze");
         }
         else
@@ -46,8 +54,17 @@ public class MazeExitPath {
     boolean isValidIndex(int maze[][], int x, int y, int n, int m)
     {
         // if (x,y outside maze) return false
-        return (x >= 0 && x < n && y >= 0 &&
-                y < m && maze[x][y] == 1);
+//        return (x >= 0 && x < n && y >= 0 &&
+//                y < m && maze[x][y] == 1);
+    	
+        if (x >= 0 && x < n && y >= 0 &&
+                y < m && maze[x][y] == 1 && !track[x][y]){
+        	
+        	track[x][y] = true;
+        	return true;
+        }
+        
+        return false;
     }
  
     /* This function solves the Maze problem using
@@ -57,9 +74,11 @@ public class MazeExitPath {
        prints the path in the form of 1s. Please note
        that there may be more than one solutions, this
        function prints one of the feasible solutions.*/
+    boolean track[][];
     boolean findExitPath(int maze[][], int n, int m)
     {
     	int sol[][] = new int [n][m];
+    	track = new boolean [n][m];
     	
         if (exitPathUtil(maze, 0, 0, sol, n, m) == false)
         {
@@ -70,8 +89,7 @@ public class MazeExitPath {
         return true;
     }
  
-    /* A recursive utility function to solve Maze
-       problem */
+    /* A recursive utility function to solve Maze problem */
     boolean exitPathUtil(int maze[][], int x, int y,
                           int sol[][], int n, int m)
     {
@@ -88,48 +106,82 @@ public class MazeExitPath {
             // mark x,y as part of solution path
             sol[x][y] = 1;
  
-            
-            //System.out.print(x + ", " + y + " = " + maze[x][y] + " | ");
-            
-            /* Move forward in x direction */
+            /* Move forward in x direction. Moving down */
             if (exitPathUtil(maze, x + 1, y, sol, n, m))
                 return true;
  
             /* If moving in x direction doesn't give
-               solution then Move down in y direction */
-            if (exitPathUtil(maze, x, y + 1, sol, n, m))
-                return true;
+               solution then in y direction. Move right */
+            else if (exitPathUtil(maze, x, y + 1, sol, n, m))
+                return true;   
+            /* Moving up in straight direction*/
+            else if (exitPathUtil(maze, x - 1, y , sol, n, m))
+                return true; 
+            /* Moving up and left direction*/
+            else if (exitPathUtil(maze, x - 1, y-1 , sol, n, m))
+                return true;            
             
             /* If none of the above movements work then
-               BACKTRACK: unmark x,y as part of solution
-               path */
+               BACKTRACK: unmark x,y as part of solution path */            
             sol[x][y] = 0;
             return false;
         }
-//        else if (isValidIndex(maze, x-1, y, n, m) == true){        
-//        	
-//        	System.out.print((x -1) + ", " + y + " = " + maze[x-1][y] + " | ");
-//        	
-//            if (solveMazeUtil(maze, x-1, y, sol, n, m))
-//                return true;
-//            
-//            if (solveMazeUtil(maze, x-1, y-1, sol, n, m))
-//                return true;            
-//            
-//            sol[x-1][y] = 0;
-//            return false;
-//        }
-//        else if (isValidIndex(maze, x-1, y-1, n, m) == true){        
-//        	
-//        	System.out.print((x -1) + ", " + (y-1) + " = " + maze[x-1][y-1] + " | ");        	
-//           
-//            if (solveMazeUtil(maze, x-1, y-1, sol, n, m))
-//                return true;            
-//            
-//            sol[x-1][y-1] = 0;
-//            return false;
-//        }
         
         return false;
     }
+    
+    boolean isValidIndexOnlyRightAndDownMove(int maze[][], int x, int y, int n, int m)
+    {
+        // if (x,y outside maze) return false
+        return (x >= 0 && x < n && y >= 0 &&
+                y < m && maze[x][y] == 1);
+
+    }    
+    
+    boolean findExitPathOnlyRightAndDownMove(int maze[][], int n, int m) {
+    	int sol[][] = new int [n][m];
+    	
+        if (exitPathUtilOnlyRightAndDownMove(maze, 0, 0, sol, n, m) == false)
+        {
+            return false;
+        }
+ 
+        printPath(sol, n, m);
+        return true;
+    }
+    
+    boolean exitPathUtilOnlyRightAndDownMove(int maze[][], int x, int y, int sol[][], int n, int m) {
+		// if (x,y is goal) return true
+		if (x == n - 1 && y == m - 1)
+		{
+		sol[x][y] = 1;
+		return true;
+		}
+		
+		// Check if maze[x][y] is valid
+		if (isValidIndexOnlyRightAndDownMove(maze, x, y, n, m) == true) {
+			// mark x,y as part of solution path
+			sol[x][y] = 1;
+			
+			
+			//System.out.print(x + ", " + y + " = " + maze[x][y] + " | ");
+			
+			/* Move forward in x direction. Moving down */
+			if (exitPathUtilOnlyRightAndDownMove(maze, x + 1, y, sol, n, m))
+			  return true;
+			
+			/* If moving in x direction doesn't give
+			 solution then in y direction. Move right */
+			else if (exitPathUtilOnlyRightAndDownMove(maze, x, y + 1, sol, n, m))
+			  return true;   
+          
+			
+			/* If none of the above movements work then
+			 BACKTRACK: unmark x,y as part of solution path */            
+			sol[x][y] = 0;
+			return false;
+		}
+		
+		return false;
+	}    
 }
